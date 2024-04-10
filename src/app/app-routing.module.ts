37c8aa0cda3c3b80c18198f36fa912a0cf97cbc9
amczuboka <94947740+amczuboka@users.noreply.github.com>
2024-04-10 01:07:12 -0400

    Create points component
    Create uncompleted points component and add a path to routing to route to this component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { PointsComponent } from './pages/points/points.component';
 
 const routes: Routes = [
+  {
+    path: 'points',
+    component: PointsComponent,
+  },
   {
     path: '',
     component: LandingComponent,
---
+++ src/app/pages/points/points.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-points',
+  templateUrl: './points.component.html',
+  styleUrl: './points.component.scss'
+})
+export class PointsComponent {
+
+}
