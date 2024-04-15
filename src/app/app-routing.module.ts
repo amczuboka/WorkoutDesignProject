367e4c47e8e32eb3fe806ce5440d86d25582eb29
Walid <walidoo_2002@hotmail.com>
2024-04-15 01:13:50 -0400

    feat(core): add workouts page to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { PointsComponent } from './pages/points/points.component';
+import { WorkoutsComponent } from "./pages/workouts/workouts.component";
 
 const routes: Routes = [
   {
@@ -12,10 +13,15 @@ const routes: Routes = [
     path: '',
     component: LandingComponent,
   },
+  {
+    path: 'workouts',
+    component: WorkoutsComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
